55c12d1a4f667f6210056cdcf5c5bddd7fa5684d
ncaklovic <nenad.caklovic@gmail.com>
2015-11-11 18:08:14 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "rc.h"
+
+int sin_dialog::idd() const {
+	return IDD_LEGEND; 
+}
+
+bool sin_dialog::on_init_dialog(){
+	return true;
+}
+
+bool sin_dialog::on_ok(){
+	return true;
+}
+
+void main_window::on_paint(HDC hdc){
+}
+
+void main_window::on_command(int id){
+	switch(id){
+		case ID_LEGEND: 
+			break;
+		case ID_EXIT: 
+			::DestroyWindow(*this); 
+			break;
+	}
+}
+
+void main_window::on_destroy(){
+	::PostQuitMessage(0);
+} 
+
+
+int WINAPI WinMain(HINSTANCE hi, HINSTANCE, LPSTR, int)
+{
+	vsite::nwp::application app;
+	main_window wnd;
+	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (UINT_PTR)LoadMenu(hi, MAKEINTRESOURCE(IDM_MAIN)));
+	return app.run();
+}
