5aa992c5dd17db92bd15be27eb68d3442722eab0
skulenov <skulenov@gmail.com>
2015-11-23 00:38:19 +0100

    U OnOK() pokupljene vrijednosti sa kontrola. Dodan nedostajuci throw u GetReal(). Dodani neki clanovi u MainWindow class i konstruktor za MainWindow.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ bool SinDialog::OnInitDialog(){
 }
 
 bool SinDialog::OnOK(){
+	try
+	{
+		SinDialog::period = SinDialog::GetReal(IDC_EDIT1);
+		SinDialog::legText = SinDialog::GetText(IDC_EDIT2);
+	}
+	catch (XCtrl ex)
+	{
+		return false;
+	}
 	return true;
 }
 
@@ -22,16 +31,20 @@ void MainWindow::OnPaint(HDC hdc){
 	LineTo(hdc, rect.right, rect.bottom / 2);
 	MoveToEx(hdc, rect.right / 2, 0,NULL);
 	LineTo(hdc, rect.right / 2, rect.bottom);
+
+
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_LEGEND: 
+			
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
 	}
+	InvalidateRect(*this, NULL, TRUE);
 }
 
 void MainWindow::OnDestroy(){
--- main.h
+++ main.h
@@ -3,9 +3,11 @@
 
 #include "nwpwin.h"
 #include "nwpdlg.h"
-
+#include <vector>
 
 class SinDialog : public Dialog {
+	double period;
+	tstring legText;
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -13,6 +15,16 @@ protected:
 };
 
 class MainWindow : public Window {
+	std::vector<POINT> points;
+	tstring sText;
+	LOGFONT logFont;
+	double dPeriod;
+public:
+	MainWindow() : dPeriod(0), sText("Legend")
+	{
+		::ZeroMemory(&logFont, sizeof(logFont));
+		_tcscpy(logFont.lfFaceName, _T("Arial"));
+	}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
