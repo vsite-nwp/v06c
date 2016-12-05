ea919fb2ed5bc7718cfdb77a7c1ab561a3a2cf5b
kapusta <kapusta.borna.s@gmail.com>
2016-12-05 20:29:34 +0100

    Napravljen dialog sa setovima i getovima za iscrtavanje nove sinusoide

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "main.h"
 #include "rc.h"
 
-
 int SinDialog::IDD(){
 	return IDD_LEGEND; 
 }
@@ -14,52 +13,52 @@ bool SinDialog::OnInitDialog(){
 
 bool SinDialog::OnOK() {
 	try {
-		period = GetInt(IDC_EDIT1);
+		period = GetReal(IDC_EDIT1);
 		txtLegenda = GetText(IDC_EDIT2);
 	}
 	catch (XCtrl&) {
-		MessageBox(*this, "Aj ti opet", "Krivi unos", MB_OK | MB_SETFOREGROUND);
-		return true;
+		MessageBox(*this, "Nepravilni unos, pokušaj ponovo", "Krivi unos", MB_OK | MB_ICONWARNING);
+		return false;
 	}
 }
 
 void MainWindow::OnPaint(HDC hdc){
 	RECT rect;
 	GetClientRect(*this, &rect);
-
-	//x os
+	DrawText(hdc, text.c_str(), -1, &rect, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+		//x os
 	MoveToEx(hdc, 0, rect.bottom / 2, NULL);
 	LineTo(hdc, rect.right, rect.bottom / 2);
-	//y os
+		//y os
 	MoveToEx(hdc, rect.right / 2, 0, NULL);
 	LineTo(hdc, rect.right / 2, rect.bottom);
 	
 	MoveToEx(hdc, rect.left, (1 - sin(rect.right / 2 * xperiod / rect.right))*rect.bottom /2, NULL);
+
 	for (int x = 0, y = 0; x <= rect.right; ++x)
 	{
 		y = (1 - sin((x - rect.right / 2)*xperiod / rect.right))*rect.bottom/2;
 		LineTo(hdc, x, y);
 	}
-	
-	DrawText(hdc, text.c_str(), -1, &rect, DT_RIGHT | DT_TOP | DT_SINGLELINE);
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
-	case ID_LEGEND: {
-		SinDialog sinDialog;
-		sinDialog.period = xperiod;
-		sinDialog.txtLegenda = text;
-		if (sinDialog.DoModal(0, *this) == IDOK) {
-			xperiod = sinDialog.period;
-			text = sinDialog.txtLegenda;
+		case ID_LEGEND: {
+			SinDialog sinDialog;
+			sinDialog.period = xperiod;
+			sinDialog.txtLegenda = text;
+			if (sinDialog.DoModal(0, *this) == IDOK) {
+				xperiod = sinDialog.period;
+				text = sinDialog.txtLegenda;
+			}
 		}
-	}
+			InvalidateRect(*this, NULL, true);
 			break;
-	case ID_EXIT: 
-		DestroyWindow(*this); 
-		break;
-	}
+		case ID_EXIT: 
+			DestroyWindow(*this); 
+			break;
+		}
 }
 
 void MainWindow::OnDestroy(){
