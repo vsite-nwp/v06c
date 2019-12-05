0ca46024612cdd47e432a1c680c067fde492b5d2
Domagoj Salkić <48859348+domagojsalkic@users.noreply.github.com>
2019-12-05 01:45:03 +0100

    Finished exercise

--- main.cpp
+++ main.cpp
@@ -1,43 +1,73 @@
 #include "main.h"
 #include "rc.h"
 
-int SinDialog::IDD(){
-	return IDD_LEGEND; 
+int SinDialog::IDD()
+{
+	return IDD_LEGEND;
 }
 
-bool SinDialog::OnInitDialog(){
+bool SinDialog::OnInitDialog()
+{
+	SetReal(IDC_EDIT1, sinPeriod);
+	SetText(IDC_EDIT2, dialText);
 	return true;
 }
 
-bool SinDialog::OnOK(){
+bool SinDialog::OnOK()
+{
+	sinPeriod = GetReal(IDC_EDIT1);
+	dialText = GetText(IDC_EDIT2);
 	return true;
 }
 
 
-void MainWindow::OnPaint(HDC hdc){
-}
-
-void MainWindow::OnCommand(int id){
-	switch(id){
-		case ID_LEGEND: 
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+void MainWindow::OnPaint(HDC hdc) {
+	RECT rect;
+	GetClientRect(*this, &rect);
+	DrawText(hdc, winText.c_str(), -1, &rect, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+	MoveToEx(hdc, 0, rect.bottom / 2, NULL);
+	LineTo(hdc, rect.right, rect.bottom / 2);
+	MoveToEx(hdc, rect.right / 2, 0, NULL);
+	LineTo(hdc, rect.right / 2, rect.bottom);
+	MoveToEx(hdc, rect.left, rect.bottom / 2, NULL);
+	for (double x = 0, y = 0; x <= rect.right; x++)
+	{
+		y = (1 - sin((x - (rect.right / 2)) * winPeriod / rect.right)) * rect.bottom / 2;
+		LineTo(hdc, x, y);
 	}
 }
 
-void MainWindow::OnDestroy(){
-	::PostQuitMessage(0);
-} 
+void MainWindow::OnCommand(int id) {
+	switch (id) {
+	case ID_LEGEND:
+	{
+		SinDialog sd;
+		sd.SetDialPeriod(winPeriod);
+		sd.SetDialText(winText);
+		if (sd.DoModal(0, *this) == IDOK)
+		{
+			winPeriod = sd.GetSinPeriod();
+			winText = sd.GetDialText();
+		}
+	}
+	InvalidateRect(*this, NULL, true);
+	break;
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
+	}
+}
 
+void MainWindow::OnDestroy() {
+	::PostQuitMessage(0);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	Application app;
-	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
-		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
+	MainWindow wnd;
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP",
+		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.Run();
 }
 
--- main.h
+++ main.h
@@ -1,21 +1,34 @@
 #include <windows.h>
 #include <tchar.h>
+#include <math.h>
 
 #include "nwpwin.h"
 #include "nwpdlg.h"
 
 
 class SinDialog : public Dialog {
+	tstring dialText;
+	double sinPeriod;
 protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
+public:
+	void SetDialText(tstring str) { dialText = str; }
+	tstring GetDialText() { return dialText; }
+	void SetDialPeriod(double period) { sinPeriod = period; }
+	double GetSinPeriod() { return sinPeriod; }
+
 };
 
 class MainWindow : public Window {
+	tstring winText;
+	double winPeriod;
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+public:
+	MainWindow() : winPeriod(0.0), winText("") {}
 };
  
