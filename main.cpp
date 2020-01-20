5375fa91c4b73a9df40f7bad7577db26ec4f0096
Marko Bohač <mabohac@vsite.hr>
2020-01-20 17:59:34 +0100

    Gotova vjezba

--- main.cpp
+++ main.cpp
@@ -6,23 +6,57 @@ int SinDialog::IDD(){
 }
 
 bool SinDialog::OnInitDialog(){
+	SetReal(IDC_EDIT1, sin);
+	SetText(IDC_EDIT2, legend);
 	return true;
 }
 
 bool SinDialog::OnOK(){
+	try
+	{
+		sin = GetReal(IDC_EDIT1);
+		legend = GetText(IDC_EDIT2);
+	}
+	catch (XCtrl&)
+	{
+		return false;
+	}
 	return true;
 }
 
 
 void MainWindow::OnPaint(HDC hdc){
+	RECT graph;
+	GetClientRect(*this, &graph);
+	DrawText(hdc, periodTxt.c_str(), -1, &graph, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+	MoveToEx(hdc, 0, graph.bottom / 2, NULL);
+	LineTo(hdc, graph.right, graph.bottom / 2);
+	MoveToEx(hdc, graph.right / 2, 0, NULL);
+	LineTo(hdc, graph.right / 2, graph.bottom);
+	MoveToEx(hdc, graph.left, graph.bottom / 2, NULL);
+	for (double x = 0, y = 0; x <= graph.right; x++)
+	{
+		y = (1 - sin((x - (graph.right / 2)) * period / graph.right)) * graph.bottom / 2;
+		LineTo(hdc, x, y);
+	}
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
-		case ID_LEGEND: 
-			break;
+		case ID_LEGEND:
+		{
+			SinDialog dialog;
+			dialog.SetDialPeriod(period);
+			dialog.SetDialText(periodTxt);
+			if (dialog.DoModal(0, *this) == IDOK){
+				period = dialog.GetSinPeriod();
+				periodTxt = dialog.GetDialText();
+				InvalidateRect(*this, NULL, true);
+			}
+		}
+		break;
 		case ID_EXIT: 
-			DestroyWindow(*this); 
+			DestroyWindow(*this);
 			break;
 	}
 }
--- main.h
+++ main.h
@@ -3,19 +3,33 @@
 
 #include "nwpwin.h"
 #include "nwpdlg.h"
-
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 class SinDialog : public Dialog {
+private:
+	tstring legend;
+	double sin;
 protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
+public:
+	void SetDialText(tstring str) { legend = str; }
+	tstring GetDialText() { return legend; }
+	void SetDialPeriod(double period) { sin = period; }
+	double GetSinPeriod() { return sin; }
 };
 
 class MainWindow : public Window {
+private:
+	tstring periodTxt;
+	double period;
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+public:
+	MainWindow() : period(2 * M_PI) {}
 };
  
