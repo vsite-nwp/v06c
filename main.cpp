d9a990eb5e6a8854606343a0cf76f406c777d015
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2021-03-29 12:02:16 +0200

    Update project, add namespace, rename methods

--- main.cpp
+++ main.cpp
@@ -1,43 +1,40 @@
 #include "main.h"
 #include "rc.h"
 
-int SinDialog::IDD(){
+int sin_dialog::idd() const {
 	return IDD_LEGEND; 
 }
 
-bool SinDialog::OnInitDialog(){
+bool sin_dialog::on_init_dialog(){
 	return true;
 }
 
-bool SinDialog::OnOK(){
+bool sin_dialog::on_ok(){
 	return true;
 }
 
-
-void MainWindow::OnPaint(HDC hdc){
+void main_window::on_paint(HDC hdc){
 }
 
-void MainWindow::OnCommand(int id){
+void main_window::on_command(int id){
 	switch(id){
 		case ID_LEGEND: 
 			break;
 		case ID_EXIT: 
-			DestroyWindow(*this); 
+			::DestroyWindow(*this); 
 			break;
 	}
 }
 
-void MainWindow::OnDestroy(){
+void main_window::on_destroy(){
 	::PostQuitMessage(0);
 } 
 
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
-	Application app;
-	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
-		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
-	return app.Run();
+	vsite::nwp::application app;
+	main_window wnd;
+	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(instance, MAKEINTRESOURCE(IDM_MAIN)));	
+	return app.run();
 }
-
--- main.h
+++ main.h
@@ -1,21 +1,16 @@
-#include <windows.h>
-#include <tchar.h>
-
 #include "nwpwin.h"
 #include "nwpdlg.h"
 
-
-class SinDialog : public Dialog {
+class sin_dialog : public vsite::nwp::dialog {
 protected:
-	int IDD();
-	bool OnInitDialog();
-	bool OnOK();
+	int idd() const override;
+	bool on_init_dialog() override;
+	bool on_ok() override;
 };
 
-class MainWindow : public Window {
+class main_window : public vsite::nwp::window {
 protected:
-	void OnPaint(HDC hdc);
-	void OnCommand(int id);
-	void OnDestroy();
+	void on_paint(HDC hdc) override;
+	void on_command(int id) override;
+	void on_destroy() override;
 };
- 
