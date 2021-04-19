f651ef8d3fc0e87598ccfac23d2893036be8604b
Paolo Stabile <pastabil@vsite.hr>
2021-04-19 17:17:35 +0200

    Resolve v06c

--- main.cpp
+++ main.cpp
@@ -6,20 +6,55 @@ int sin_dialog::idd() const {
 }
 
 bool sin_dialog::on_init_dialog(){
+	set_real(IDC_EDIT1, sin);
+	set_text(IDC_EDIT2, legend);
 	return true;
 }
 
 bool sin_dialog::on_ok(){
+	try
+	{
+		sin = get_real(IDC_EDIT1);
+		legend = get_text(IDC_EDIT2);
+	}
+	catch (std::exception& error)
+	{
+		MessageBoxA(*this, error.what(), "Error", MB_OK | MB_ICONWARNING);
+		return false;
+	}
 	return true;
 }
 
 void main_window::on_paint(HDC hdc){
+	RECT graph;
+	GetClientRect(*this, &graph);
+	DrawText(hdc, periodTxt.c_str(), -1, &graph, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+	MoveToEx(hdc, 0, graph.bottom / 2, NULL);
+	LineTo(hdc, graph.right, graph.bottom / 2);
+	MoveToEx(hdc, graph.right / 2, 0, NULL);
+	LineTo(hdc, graph.right / 2, graph.bottom);
+	MoveToEx(hdc, graph.left, graph.bottom / 2, NULL);
+	for (double x = 0, y = 0; x <= graph.right; x++)
+	{
+		y = (1 - sin((x - (graph.right / 2)) * period / graph.right)) * graph.bottom / 2;
+		LineTo(hdc, x, y);
+	}
 }
 
 void main_window::on_command(int id){
 	switch(id){
 		case ID_LEGEND: 
-			break;
+		{
+			sin_dialog dialog;
+			dialog.set_dial_period(period);
+			dialog.set_dial_text(periodTxt);
+			if (dialog.do_modal(0, *this) == IDOK) {
+				period = dialog.get_sin_period();
+				periodTxt = dialog.get_dial_text();
+				InvalidateRect(*this, NULL, true);
+			}
+		}
+		break;
 		case ID_EXIT: 
 			::DestroyWindow(*this); 
 			break;
--- main.h
+++ main.h
@@ -1,16 +1,31 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 class sin_dialog : public vsite::nwp::dialog {
+private:
+	tstring legend;
+	double sin;
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+public:
+	void set_dial_text(tstring str) { legend = str; }
+	tstring get_dial_text() { return legend; }
+	void set_dial_period(double period) { sin = period; }
+	double get_sin_period() { return sin; }
 };
 
 class main_window : public vsite::nwp::window {
+private:
+	tstring periodTxt;
+	double period;
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+public:
+	main_window() : period(2 * M_PI) {}
 };
