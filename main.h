55a50174a3fd67c213533ee69569e9c9a986d115
Vlado Tonković <vltonkov@vsite.hr>
2022-04-11 19:24:30 +0200

    Resolve v06c

--- main.h
+++ main.h
@@ -1,16 +1,33 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-class sin_dialog : public vsite::nwp::dialog {
+class sin_dialog : public vsite::nwp::dialog 
+{
+private:
+	tstring legend;
+	double sin;
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+public:
+	void SetLegendText(tstring str) { legend = str; }
+	tstring GetLegendText() { return legend; }
+	void SetSinInterval(double interval) { sin = interval; }
+	double GetSinInterval() { return sin; }
 };
 
-class main_window : public vsite::nwp::window {
+class main_window : public vsite::nwp::window
+{
+private:
+	tstring interval_text;
+	double interval;
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+public:
+	main_window() : interval(2 * M_PI) {}
 };
