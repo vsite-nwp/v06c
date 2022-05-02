303ac1c1a43ab7fc8e44300298b42042ead8d01d
robilovric <robilovric7@gmail.com>
2022-05-02 15:54:39 +0200

    Resolve all

--- main.cpp
+++ main.cpp
@@ -6,19 +6,53 @@ int sin_dialog::idd() const {
 }
 
 bool sin_dialog::on_init_dialog(){
+	set_real(IDC_EDIT1, sin);
+	set_text(IDC_EDIT2, legend);
 	return true;
 }
 
 bool sin_dialog::on_ok(){
+	try
+	{
+		sin = get_real(IDC_EDIT1);
+		legend = get_text(IDC_EDIT2);
+	}
+	catch (std::runtime_error &err)
+	{
+		return false;
+	}
 	return true;
 }
 
 void main_window::on_paint(HDC hdc){
+	RECT graph;
+	GetClientRect(*this, &graph);
+	DrawText(hdc, description.c_str(), -1, &graph, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+	MoveToEx(hdc, 0, graph.bottom / 2, NULL);
+	LineTo(hdc, graph.right, graph.bottom / 2);
+	MoveToEx(hdc, graph.right / 2, 0, NULL);
+	LineTo(hdc, graph.right / 2, graph.bottom);
+	MoveToEx(hdc, graph.left, graph.bottom / 2, NULL);
+	for (double x = 0, y = 0; x <= graph.right; x++)
+	{
+		y = (1 - sin((x - (graph.right / 2)) * period / graph.right)) * graph.bottom / 2;
+		LineTo(hdc, x, y);
+	}
 }
 
 void main_window::on_command(int id){
 	switch(id){
 		case ID_LEGEND: 
+		{
+			sin_dialog dlg;
+			dlg.SetDialPeriod(period);
+			dlg.SetDialText(description);
+			if (dlg.do_modal(0, *this) == IDOK) {
+				period = dlg.GetSinPeriod();
+				description = dlg.GetDialText();
+				InvalidateRect(*this, NULL, true);
+			}
+		}
 			break;
 		case ID_EXIT: 
 			::DestroyWindow(*this); 
--- main.h
+++ main.h
@@ -1,11 +1,21 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 class sin_dialog : public vsite::nwp::dialog {
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+private:
+	tstring legend;
+	double sin;
+public:
+	void SetDialText(tstring str) { legend = str; }
+	tstring GetDialText() { return legend; }
+	void SetDialPeriod(double period) { sin = period; }
+	double GetSinPeriod() { return sin; }
 };
 
 class main_window : public vsite::nwp::window {
@@ -13,4 +23,9 @@ protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+private:
+	tstring description;
+	double period;
+public:
+	main_window() : period(2 * M_PI) {}
 };
