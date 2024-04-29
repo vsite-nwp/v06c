7ab90cd96ab4ffb3c74bd56e692438b90d521356
Josip Grabušić <jograbus@vsite.hr>
2024-04-29 18:00:58 +0200

    Vježba 6c - riješena

--- main.cpp
+++ main.cpp
@@ -1,40 +1,78 @@
 #include "main.h"
 #include "rc.h"
 
-int sin_dialog::idd() const {
-	return IDD_LEGEND; 
+int sin_dialog::idd() const 
+{
+	return IDD_LEGEND;
 }
 
-bool sin_dialog::on_init_dialog(){
+bool sin_dialog::on_init_dialog() 
+{
+	set_real(IDC_EDIT1, sin);
+	set_text(IDC_EDIT2, legend);
 	return true;
 }
 
-bool sin_dialog::on_ok(){
+bool sin_dialog::on_ok() 
+{
+	sin = get_real(IDC_EDIT1);
+	legend = get_text(IDC_EDIT2);
 	return true;
 }
 
-void main_window::on_paint(HDC hdc){
-}
+void main_window::on_paint(HDC hdc) 
+{
+	RECT rc;
+	GetClientRect(*this, &rc);
+	DrawText(hdc, tekst.c_str(), -1, &rc, DT_RIGHT | DT_TOP | DT_SINGLELINE);
 
-void main_window::on_command(int id){
-	switch(id){
-		case ID_LEGEND: 
-			break;
-		case ID_EXIT: 
-			::DestroyWindow(*this); 
-			break;
+	MoveToEx(hdc, rc.right / 2, rc.top, NULL);   
+	LineTo(hdc, rc.right / 2, rc.bottom);           
+	MoveToEx(hdc, rc.left, rc.bottom / 2, NULL);     
+	LineTo(hdc, rc.right, rc.bottom / 2);            
+	
+	MoveToEx(hdc, rc.left, (1 - sin((rc.left - rc.right / 2.0) * period / rc.right)) * rc.bottom / 2, NULL);
+	for (double x = rc.left, y = 0; x <= rc.right; x++)
+	{
+		y = (1 - sin((x - (rc.right / 2)) * period / rc.right)) * rc.bottom / 2;
+		LineTo(hdc, x, y);
 	}
 }
 
-void main_window::on_destroy(){
+void main_window::on_command(int id) 
+{
+	switch (id) 
+	{
+	case ID_LEGEND:
+	{
+		sin_dialog dlg;
+		dlg.SetPeriod(period);
+		dlg.SetText(tekst);
+		if (dlg.do_modal(0, *this) == IDOK) 
+		{
+			period = dlg.GetPeriod();
+			tekst = dlg.GetText();
+			InvalidateRect(*this, NULL, true);
+		}
+	}
+	break;
+
+	case ID_EXIT:
+		::DestroyWindow(*this);
+		break;
+	}
+}
+
+void main_window::on_destroy() 
+{
 	::PostQuitMessage(0);
-} 
+}
 
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	vsite::nwp::application app;
 	main_window wnd;
-	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(instance, MAKEINTRESOURCE(IDM_MAIN)));	
+	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(instance, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.run();
 }
--- main.h
+++ main.h
@@ -1,16 +1,52 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-class sin_dialog : public vsite::nwp::dialog {
+class sin_dialog : public vsite::nwp::dialog 
+{
+public:
+	void SetText(tstring str) 
+	{
+		legend = str; 
+	}
+	
+	tstring GetText() 
+	{ 
+		return legend; 
+	}
+	void SetPeriod(double period) 
+	{ 
+		sin = period; 
+	}
+	
+	double GetPeriod() 
+	{ 
+		return sin; 
+	}
+
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+
+private:
+	tstring legend;
+	double sin;
+
 };
 
-class main_window : public vsite::nwp::window {
+class main_window : public vsite::nwp::window 
+{
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+
+private:
+	tstring tekst;
+	double period;
+	
+public:
+	main_window() : period(2 * M_PI) {} 
 };
