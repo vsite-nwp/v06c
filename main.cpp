d3ecada9a5719ac34bfe7f3f9650a6d561f1a907
Vlado Tonković <vltonkov@vsite.hr>
2024-06-03 22:00:29 +0200

    Resolve v06c

--- main.cpp
+++ main.cpp
@@ -1,40 +1,92 @@
 #include "main.h"
 #include "rc.h"
 
-int sin_dialog::idd() const {
-	return IDD_LEGEND; 
+double CalcY(RECT r, double interval, double x)
+{
+	return (1 - sin((x - r.right / 2) * interval / r.right)) * r.bottom / 2;
 }
 
-bool sin_dialog::on_init_dialog(){
+int sin_dialog::idd() const
+{
+	return IDD_LEGEND;
+}
+
+bool sin_dialog::on_init_dialog()
+{
+	set_real(IDC_EDIT1, sin);
+	set_text(IDC_EDIT2, legend);
 	return true;
 }
 
-bool sin_dialog::on_ok(){
+bool sin_dialog::on_ok()
+{
+	try
+	{
+		sin = get_real(IDC_EDIT1);
+		legend = get_text(IDC_EDIT2);
+	}
+	catch (std::exception)
+	{
+		MessageBox(*this, L"Invalid input", nullptr, MB_OK | MB_ICONERROR);
+		return false;
+	}
 	return true;
 }
 
-void main_window::on_paint(HDC hdc){
+void main_window::on_paint(HDC hdc)
+{
+	RECT graph;
+	GetClientRect(*this, &graph);
+	DrawText(hdc, interval_text.c_str(), -1, &graph, DT_RIGHT | DT_TOP | DT_SINGLELINE);
+
+	int graph_height = graph.bottom / 2;
+	int graph_width = graph.right / 2;
+
+	MoveToEx(hdc, 0, graph_height, nullptr);
+	LineTo(hdc, graph.right, graph_height);
+	MoveToEx(hdc, graph_width, 0, nullptr);
+	LineTo(hdc, graph_width, graph.bottom);
+	MoveToEx(hdc, graph.left, CalcY(graph, interval, 0), nullptr);
+
+	for (double x = graph.left, y = 0; x <= graph.right; x++)
+	{
+		y = CalcY(graph, interval, x);
+		LineTo(hdc, x, y);
+	}
 }
 
-void main_window::on_command(int id){
-	switch(id){
-		case ID_LEGEND: 
-			break;
-		case ID_EXIT: 
-			::DestroyWindow(*this); 
+void main_window::on_command(int id)
+{
+	switch (id)
+	{
+		case ID_LEGEND:
+		{
+			sin_dialog dialog;
+			dialog.SetSinInterval(interval);
+			dialog.SetLegendText(interval_text);
+			if (dialog.do_modal(0, *this) == IDOK)
+			{
+				interval = dialog.GetSinInterval();
+				interval_text = dialog.GetLegendText();
+				InvalidateRect(*this, nullptr, true);
+			}
+		}
+		break;
+		case ID_EXIT:
+			::DestroyWindow(*this);
 			break;
 	}
 }
 
-void main_window::on_destroy(){
+void main_window::on_destroy()
+{
 	::PostQuitMessage(0);
-} 
-
+}
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	vsite::nwp::application app;
 	main_window wnd;
-	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(instance, MAKEINTRESOURCE(IDM_MAIN)));	
+	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(instance, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.run();
 }
